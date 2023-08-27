4863f3f40d02411b3f438aa8978905a6200e4826
Kachirou <hoang286272@gmail.com>
2023-08-27 08:41:37 +0700

    up

--- server/src/services/auth.js
+++ server/src/services/auth.js
@@ -35,3 +35,33 @@ export const registerService = ({ phone, password, name }) =>
       reject(error);
     }
   });
+
+export const loginService = ({ phone, password }) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.User.findOne({
+        where: { phone },
+        raw: true,
+      });
+      const isCorrectPassword =
+        response && bcrypt.compareSync(password, response.password);
+      const token =
+        isCorrectPassword &&
+        jwt.sign(
+          { id: response.id, phone: response.phone },
+          process.env.SECRET_KEY,
+          { expiresIn: "2d" }
+        );
+      resolve({
+        err: token ? 0 : 2,
+        msg: token
+          ? "Login successfully !"
+          : response
+          ? "Password is wrong !"
+          : "phone number not found !",
+        token: token || null,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
