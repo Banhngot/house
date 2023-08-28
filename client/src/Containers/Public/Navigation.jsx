5e8be0e4703e4ff9b68d44cc9a2baf2304baddf2
Kachirou <hoang286272@gmail.com>
2023-08-28 12:19:19 +0700

    up

--- client/src/Containers/Public/Navigation.jsx
+++ client/src/Containers/Public/Navigation.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+const nav = [
+  { name: "Trang chủ", path: "home" },
+  { name: "Cho thuê phòng trọ", path: "cho-thue-phong-tro" },
+  { name: "Nhà cho thuê", path: "nha-cho-thue" },
+  { name: "Cho thuê căn hộ", path: "cho-thue-can-ho" },
+  { name: "Cho thuê mặt bằng", path: "cho-thue-mat-bang" },
+];
+
+const notActive = "hover:bg-[#FFBA86] py-2 px-4";
+const active = "hover:bg-[#FFBA86] py-2 px-4 bg-[#FFBA86]";
 
 const Navigation = () => {
   return (
-    <div className="w-screen flex justify-center items-center h-[40px] bg-secondary1 text-white">
-      <div className="w-1100">Navigation</div>
+    <div className="w-screen flex justify-center items-center h-[40px]  bg-secondary1 text-white">
+      <div className="w-1100 flex items-center gap-3 text-black text-sm font-medium">
+        {nav?.length > 0 &&
+          nav.map((item, index) => {
+            return (
+              <div key={index}>
+                <NavLink
+                  to={item.path}
+                  className={({ isActive }) => (isActive ? active : notActive)}
+                >
+                  {item.name}
+                </NavLink>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
