a196f5afb957a974a7402ed37d09f10d3c0e8245
Kachirou <hoang286272@gmail.com>
2023-08-28 17:46:59 +0700

    up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, Login } from "./Containers/Public";
+import {
+  Home,
+  Login,
+  RentalApartment,
+  RentalHouse,
+  RentalRoom,
+  RentalSpace,
+  HomePage,
+} from "./Containers/Public";
 import { path } from "./Ultils/constant";
 function App() {
   return (
     <div className="h-screen w-screen bg-primary">
       <Routes>
         <Route path={path.HOME} element={<Home />}>
+          <Route path="*" element={<HomePage />} />
           <Route path={path.LOGIN} element={<Login />} />
+          <Route path={path.CHO_THUE_CAN_HO} element={<RentalApartment />} />
+          <Route path={path.CHO_THUE_MAT_BANG} element={<RentalSpace />} />
+          <Route path={path.CHO_THUE_PHONG_TR0} element={<RentalRoom />} />
+          <Route path={path.NHA_CHO_THUE} element={<RentalHouse />} />
         </Route>
       </Routes>
     </div>
--- server/src/services/category.js
+++ server/src/services/category.js
@@ -4,7 +4,10 @@ import db from "../models";
 export const getCategoriesService = () =>
   new Promise(async (resolve, reject) => {
     try {
-      const response = await db.Category.findAll({ raw: true });
+      const response = await db.Category.findAll({
+        raw: true,
+        attributes: ["code", "value"],
+      });
       resolve({
         err: response ? 0 : 1,
         msg: response ? "ok" : "fail",
