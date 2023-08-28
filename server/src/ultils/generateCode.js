ee55a721f5749ccb5c342a9fa98aa3d192ce8ba3
Kachirou <hoang286272@gmail.com>
2023-08-28 22:05:10 +0700

    up

--- server/src/ultils/generateCode.js
+++ server/src/ultils/generateCode.js
@@ -1,11 +1,24 @@
-const generateCode = (length) => {
-  const character = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const number = "123456789";
-  let code = "";
-  for (let i = 0; i < length - 1; i++) {
-    code += character.charAt(Math.floor(Math.random() * character.length));
+require("dotenv").config();
+
+const generateCode = (value) => {
+  let output = "";
+  value = value
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .split(" ")
+    .join("");
+  let merge = value + process.env.dreamhouse;
+  let length = merge.length;
+  // abc + dreamhouse =
+  for (let i = 0; i < 3; i++) {
+    let index =
+      i === 2
+        ? Math.floor(merge.length / 2 + length / 2)
+        : Math.floor(length / 2);
+    output += merge.charAt(index);
+    length = index;
   }
-  return `${code}${number.charAt(Math.floor(Math.random() * number.length))}`;
+  return `${value.charAt(0)}${output}`.toUpperCase();
 };
 
 export default generateCode;
