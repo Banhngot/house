bdb1493d395c419c4aa86c7a872798c90a1fd9a8
Kachirou <hoang286272@gmail.com>
2023-08-29 08:09:55 +0700

    up

--- client/src/Component/SearchItem.jsx
+++ client/src/Component/SearchItem.jsx
@@ -2,13 +2,13 @@ import React, { memo } from "react";
 
 const SearchItem = ({ IconBefore, IconAfter, text, fontWeigh }) => {
   return (
-    <div className="bg-white py-2 px-4 w-full rounded-sm text-gray-400 text-[13.3] flex items-center justify-between">
+    <div className="bg-white py-2 px-4 w-full rounded-md text-gray-400 text-[13.3] flex items-center justify-between">
       <div className="flex items-center gap-1 w-full">
         {IconBefore}
         <span
           className={`${
             fontWeigh && "font-medium text-black"
-          } w-100 overflow-hidden text-ellipsis whitespace-nowrap`}
+          } w-[100px] overflow-hidden text-ellipsis whitespace-nowrap`}
         >
           {text}
         </span>
