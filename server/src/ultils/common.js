928fbd25eda9f20edda891b523863e5313638570
Kachirou <hoang286272@gmail.com>
2023-09-01 07:51:00 +0700

    up

--- server/src/ultils/common.js
+++ server/src/ultils/common.js
@@ -1,1 +1,9 @@
-export const getNumberFromString = (string) => string.match(/\d+/)[0];
+export const getNumberFromString = (string) => {
+  let price = 0;
+  if (string.search("đồng/tháng") !== -1) {
+    price = +string.match(/\d+/)[0] / Math.pow(10, 3);
+  } else if (string.search("triệu/tháng") !== -1) {
+    price = +string.match(/\d+/)[0];
+  }
+  return price;
+};
