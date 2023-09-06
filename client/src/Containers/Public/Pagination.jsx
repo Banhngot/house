42a4fb868f6e44503dcadd1a89de9586f16da971
Kachirou <hoang286272@gmail.com>
2023-09-06 16:50:31 +0700

    up

--- client/src/Containers/Public/Pagination.jsx
+++ client/src/Containers/Public/Pagination.jsx
@@ -11,7 +11,7 @@ const Pagination = ({ page }) => {
   const [arrPage, setArrPage] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [isHideEnd, setIsHideEnd] = useState(false);
-  const [isHideStart, setisHideStart] = useState(false);
+  const [isHideStart, setIsHideStart] = useState(false);
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
@@ -22,27 +22,21 @@ const Pagination = ({ page }) => {
 
   useEffect(() => {
     let maxPage = Math.ceil(count / process.env.REACT_APP_LIMIT_POSTS);
-    let start = currentPage - 1 <= 0 ? 1 : currentPage - 1;
+    let start = currentPage - 2 <= 1 ? 1 : currentPage - 2;
 
-    let end = currentPage + 1 > maxPage ? maxPage : currentPage + 1;
+    let end = currentPage + 2 > maxPage ? maxPage : currentPage + 2;
 
     let temp = [];
     for (let i = start; i <= end; i++) temp.push(i);
     setArrPage(temp);
-    currentPage >= maxPage - 1 ? setIsHideEnd(true) : setIsHideEnd(false);
-    currentPage <= 2 ? setisHideStart(true) : setisHideStart(false);
+    currentPage >= maxPage - 2 ? setIsHideEnd(true) : setIsHideEnd(false);
+    currentPage <= 3 ? setIsHideStart(true) : setIsHideStart(false);
   }, [count, posts, currentPage]);
 
   return (
     <div className="flex items-center justify-center gap-2 py-5">
-      {!isHideStart && (
-        <PageNumber
-          icon={<GrLinkPrevious />}
-          setCurrentPage={setCurrentPage}
-          text={1}
-        />
-      )}
-      {!isHideStart && <PageNumber text={"..."} />}
+      {!isHideStart && <PageNumber setCurrentPage={setCurrentPage} text={1} />}
+      {!isHideStart && currentPage !== 4 && <PageNumber text={"..."} />}
 
       {arrPage.length > 0 &&
         arrPage.map((item) => {
