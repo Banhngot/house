4afe37ae54e5577113bf4ea64e0a9ad96778eda0
Kachirou <hoang286272@gmail.com>
2023-09-15 08:08:57 +0700

    up

--- client/src/Containers/Public/Login.jsx
+++ client/src/Containers/Public/Login.jsx
@@ -5,6 +5,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import * as actions from "../../Store/actions";
 import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
+import validate from "../../Ultils/Common/validateFields";
 
 const Login = () => {
   const location = useLocation();
@@ -37,61 +38,11 @@ const Login = () => {
           phone: payload.phone,
           password: payload.password,
         };
-    let invalids = validate(finalPayload);
+    let invalids = validate(finalPayload, setInvalidFields);
     if (invalids === 0)
       isRegister
         ? dispatch(actions.register(payload))
         : dispatch(actions.login(payload));
-  };
-
-  const validate = (payload) => {
-    let invalids = 0;
-    let fields = Object.entries(payload);
-
-    fields.forEach((item) => {
-      if (item[1] === "") {
-        setInvalidFields((prev) => [
-          ...prev,
-          {
-            name: item[0],
-            message: "You do not empty",
-          },
-        ]);
-        invalids++;
-      }
-    });
-    fields.forEach((item) => {
-      switch (item[0]) {
-        case "password":
-          if (item[1].length < 6) {
-            setInvalidFields((prev) => [
-              ...prev,
-              {
-                name: item[0],
-                message: "Password must have 6 character",
-              },
-            ]);
-            invalids++;
-          }
-          break;
-        case "phone":
-          if (!+item[1]) {
-            setInvalidFields((prev) => [
-              ...prev,
-              {
-                name: item[0],
-                message: "number invalid",
-              },
-            ]);
-            invalids++;
-          }
-          break;
-
-        default:
-          break;
-      }
-    });
-    return invalids;
   };
 
   return (
