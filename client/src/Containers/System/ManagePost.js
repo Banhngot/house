429c47f28872095b3be3af984b4b68c2f1926778
Kachirou <hoang286272@gmail.com>
2023-09-18 19:47:38 +0700

    up

--- client/src/Containers/System/ManagePost.js
+++ client/src/Containers/System/ManagePost.js
@@ -1,32 +1,83 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "../../Store/actions";
+import moment from "moment";
+import "moment/locale/vi";
 
 const ManagePost = () => {
+  const dispatch = useDispatch();
+  const { postOfCurrent } = useSelector((state) => state.post);
+
+  useEffect(() => {
+    dispatch(actions.getPostsLimitAdmin());
+  }, []);
+
+  const checkStatus = (datetime) => {
+    let todayInSeconds = new Date().getTime();
+    let expireDayInSecond = datetime.getTime();
+    return todayInSeconds >= expireDayInSecond
+      ? "Đang hoạt động"
+      : "Đã hết hạn";
+  };
+
   return (
-    <div>
+    <div className="flex flex-col gap-6">
       <div className="py-4 border-b border-gray-200 flex items-center justify-between">
         <h1 className="text-3xl font-medium ">Quản lý tin đăng</h1>
         <select className="outline-none border p-2 border-gray-200 rounded-md">
           <option value="">Lọc theo trạng thái</option>
         </select>
       </div>
-      <table className="w-full">
+      <table className="w-full table-auto">
         <thead>
           <tr>
-            <th>Mã tin</th>
-            <th>Ảnh đại diện</th>
-            <th>Tiêu đề</th>
-            <th>Giá</th>
-            <th>Ngày bắt đầu</th>
-            <th>Ngày hết hạn</th>
-            <th>Trạng thái</th>
+            <th className="border p-2">Mã tin</th>
+            <th className="border p-2">Ảnh đại diện</th>
+            <th className="border p-2">Tiêu đề</th>
+            <th className="border p-2">Giá</th>
+            <th className="border p-2">Ngày bắt đầu</th>
+            <th className="border p-2">Ngày hết hạn</th>
+            <th className="border p-2">Trạng thái</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
-          </tr>
+          {!postOfCurrent ? (
+            <tr>
+              <td>fefef</td>
+            </tr>
+          ) : (
+            postOfCurrent?.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td className="border text-center p-2">
+                    {item?.overviews?.code}
+                  </td>
+                  <td className="border flex items-center justify-center p-2">
+                    <img
+                      src={JSON.parse(item.images?.image)[0] || ""}
+                      alt="avatar-post"
+                      className="w-10 h-10 object-cover rounded-md"
+                    />
+                  </td>
+                  <td className="border text-center p-2">{item?.title}</td>
+                  <td className="border text-center p-2">
+                    {item?.attributes?.price}
+                  </td>
+                  <td className="border text-center p-2">
+                    {item?.overviews?.created}
+                  </td>
+                  <td className="border text-center p-2">
+                    {item?.overviews?.expired}
+                  </td>
+                  <td className="border text-center p-2">
+                    {checkStatus(
+                      new Date(item?.overviews?.expired?.split(" ")[3])
+                    )}
+                  </td>
+                </tr>
+              );
+            })
+          )}
         </tbody>
       </table>
     </div>
