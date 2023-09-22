366e1dc892fc26fcbe9b8dd69aeac268c732f766
Kachirou <hoang286272@gmail.com>
2023-09-22 11:09:29 +0700

    up

--- client/src/Component/InputFormV2.jsx
+++ client/src/Component/InputFormV2.jsx
@@ -16,31 +16,33 @@ const InputFormV2 = ({
       <label className="w-48 flex-none" htmlFor="title">
         {label}
       </label>
-      <div className="flex flex-auto w-full items-center">
-        <input
-          type="text"
-          id="title"
-          className={`${
-            unit ? "rounded-tl-md rounded-bl-md" : "rounded-md"
-          } outline-none border flex-auto border-gray-300 p-2`}
-          value={value}
-          onChange={(e) =>
-            setValue((prev) => ({ ...prev, [name]: e.target.value }))
-          }
-          onFocus={() => setInvalidFields([])}
-        />
-        {unit && (
-          <span className="p-2 border flex-none w-16 flex items-center justify-center rounded-tr-md rounded-br-md bg-gray-200">
-            {unit}
-          </span>
+      <div className="flex flex-auto flex-col items-center">
+        <div className="flex  w-full items-center">
+          <input
+            type="text"
+            id="title"
+            className={`${
+              unit ? "rounded-tl-md rounded-bl-md" : "rounded-md"
+            } outline-none border flex-auto border-gray-300 p-2`}
+            value={value}
+            onChange={(e) =>
+              setValue((prev) => ({ ...prev, [name]: e.target.value }))
+            }
+            onFocus={() => setInvalidFields([])}
+          />
+          {unit && (
+            <span className="p-2 border flex-none w-16 flex items-center justify-center rounded-tr-md rounded-br-md bg-gray-200">
+              {unit}
+            </span>
+          )}
+        </div>
+        {invalidFields?.some((item) => item.name === name) && (
+          <small className="text-red-500 block w-full">
+            {invalidFields?.find((item) => item.name === name)?.message}
+          </small>
         )}
       </div>
       {small && <small className="opacity-70 whitespace-nowrap">{small}</small>}
-      {invalidFields?.some((item) => item.name === name) && (
-        <small className="text-red-500 block w-full">
-          {invalidFields?.find((item) => item.name === name)?.message}
-        </small>
-      )}
     </div>
   );
 };
